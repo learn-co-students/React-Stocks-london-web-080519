19d24506d586a95da50853a5dd4e76ffb87e78a8
99chris99 <chrishodge87@gmail.com>
2019-10-14 20:02:32 +0100

    Done.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,23 @@
 import React from 'react';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
   return (
     <div>
 
       <strong>Sort by:</strong>
       <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
+        <input type="radio" value="alphabetically" checked={props.sortState == 'alphabetically' ? true : false} onChange={(event) => props.setSort('alphabetically') }/>
         Alphabetically
       </label>
       <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
+        <input type="radio" value="price" checked={props.sortState == 'price' ? true : false} onChange={(event) => props.setSort('price')}/>
         Price
       </label>
       <br/>
 
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={event => props.setFilter(event.target.value)}>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -3,12 +3,16 @@ import Stock from '../components/Stock'
 
 class PortfolioContainer extends Component {
 
+listPort = () => {
+  return this.props.port.map(stock => {return <Stock {...stock} stock={stock} trigger={this.props.trigger}/>})
+}
+
   render() {
     return (
       <div>
         <h2>My Portfolio</h2>
           {
-            //render your portfolio stocks here
+            this.listPort()
           }
       </div>
     );
