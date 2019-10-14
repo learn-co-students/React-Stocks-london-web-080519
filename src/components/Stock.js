e29eedee2450acae8cf19bd73036b3759bf2900e
george-kirby <georgeskirby@btinternet.com>
2019-10-14 18:01:34 +0100

    completed challenge

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,16 +1,12 @@
 import React from 'react'
 
-const Stock = () => (
+const Stock = (props) => (
   <div>
 
     <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">{
-            //Company Name
-          }</h5>
-        <p className="card-text">{
-            //ticker: stock price
-          }</p>
+      <div className="card-body" onClick={props.handleClick}>
+        <h5 className="card-title">{props.name}</h5>
+        <p className="card-text">{props.ticker}: {props.price}</p>
       </div>
     </div>
 
--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -7,9 +7,9 @@ class PortfolioContainer extends Component {
     return (
       <div>
         <h2>My Portfolio</h2>
-          {
-            //render your portfolio stocks here
-          }
+        {this.props.portfolio.map(stock => {
+          return <Stock {...stock} handleClick={() => this.props.removeFromPortfolio(stock)}/>
+        })}
       </div>
     );
   }
