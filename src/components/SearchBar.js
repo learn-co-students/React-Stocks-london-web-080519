3a960b48e50be34f60fc90d24fc1719b1e2fa0fe
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-15 11:09:43 +0100

    Done.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,23 @@
 import React from 'react';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
   return (
     <div>
 
       <strong>Sort by:</strong>
       <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
+        <input type="radio" value="Alphabetically" checked={props.sort === "Alphabetically"} onChange={props.sortOptions}/>
         Alphabetically
       </label>
       <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
+        <input type="radio" value="Price" checked={props.sort === "Price"} onChange={props.sortOptions}/>
         Price
       </label>
       <br/>
 
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={props.filterOptions}>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -3,23 +3,73 @@ import StockContainer from './StockContainer'
 import PortfolioContainer from './PortfolioContainer'
 import SearchBar from '../components/SearchBar'
 
+const URL = "http://localhost:3000/stocks"
+
 class MainContainer extends Component {
 
+  state = {
+    stocks: [], 
+    portfolio: [],
+    sort: "",
+    filter: "Tech"
+  }
+
+  sortOptions = (event) => {
+    this.setState({
+      sort: event.target.value
+    })
+  }
+
+
+  filterOptions = (event) => {
+    this.setState({
+      filter: event.target.value
+    })
+  }
+
+  componentDidMount() {
+    fetch(URL).then(resp=> resp.json()).then(stocks => this.setState({stocks}))
+  }
+
+  addToPortfolio = (stock) => {
+    this.setState({portfolio: [...this.state.portfolio, stock]})    
+  }
+
+  sellStock = (stock) => {
+    let removed = this.state.portfolio.findIndex(s=> s.id === stock.id)
+    this.state.portfolio.splice(removed, 1)
+    this.setState({
+      portfolio: [...this.state.portfolio]
+    })
+  }
+
+  renderOptions = () => {
+    let defaultView = this.state.stocks.filter(stock => stock.type === this.state.filter)
+    if (this.state.sort === "Alphabetically") {
+      console.log(this.state.sort)
+      return defaultView.sort((a,b) => a.ticker.localeCompare(b.ticker))
+    } else if (this.state.sort === "Price" ) {
+      return defaultView.sort((a,b) => a.price - b.price)
+    } else {
+      return defaultView
+    }
+  }
+
+
   render() {
+    const stocksToRender = this.renderOptions()
     return (
       <div>
-        <SearchBar/>
+        <SearchBar sortOptions={this.sortOptions} filterOptions={this.filterOptions} sort={this.state.sort} filter={this.state.filter}/>
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer stocks={stocksToRender} addToPortfolio={this.addToPortfolio} />
 
             </div>
             <div className="col-4">
-
-              <PortfolioContainer/>
-
+              <PortfolioContainer portfolio={this.state.portfolio} sellStock={this.sellStock} />
             </div>
           </div>
       </div>
